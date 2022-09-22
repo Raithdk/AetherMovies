3f71cd11bf41eca445606a9e09771678da03cd64
Thomas <Raithdk@gmail.com>
2022-09-22 10:20:52 +0200

    Finished Application

--- MovieDetails.js
+++ MovieDetails.js
@@ -1,13 +1,64 @@
 
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-export default function MovieDetails({navigation}){
+const api_key = "961df8cdcd64067ee9b98dfffd750c77"
 
+const image_url = "https://image.tmdb.org/t/p/w500";
+
+export default function MovieDetails(Movie){
+
+    const movieID = Movie.route.params.movieID;
+    const posterUrl = Movie.route.params.posterUrl;
+
+    const [movieData, setMovieData] = useState([]);
+
+    const url = 'https://api.themoviedb.org/3/movie/' + movieID + '?api_key='+ api_key +'&language=en-US';
+
+    useEffect(() => {
+        fetchMovie();
+    },[]);
+
+    function fetchMovie(){
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => setMovieData(data));
+    }
+    
     return (
-        <Text>HELLO</Text>
+        <View style={styles.container}>
+            <Image style={styles.posterImage} source={{uri: posterUrl}}/>
+            <Text style={styles.movieHeadline}>{movieData.original_title}</Text>
+            <Text style={styles.movieFacts}>
+                Budget: {movieData.budget} | 
+                Rating: {movieData.vote_average} | 
+                Release Status: {movieData.status}</Text>
+            <Text style={styles.movieOverview}>{movieData.overview}</Text>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    container:{
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+        margin: 20,
+    },
+    movieFacts:{
+        margin: 10,
+        fontSize: 18,
+    },
+    movieHeadline:{
+        margin:10,
+        fontSize: 26,
+    },
+    movieOverview:{
+        margin: 10,
+        fontSize: 15
+    },
+    posterImage:{
+        width: 200,
+        height: 300,
+    }
 });
--- MovieList.js
+++ MovieList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import { FlatList } from "react-native";
 import { TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
@@ -8,31 +8,44 @@ const image_url = "https://image.tmdb.org/t/p/w500";
 
 export default function MovieList({ type , url }){
     
+    // Data for all movies
     const [movieData, setMovieData] = useState([]);
-
+    const [isSorted, setIsSorted] = useState(true);
+    // When loaded, fetch all movies
     useEffect(() => {
         fetchMovies();
-
-        // TODO : Sort by popularity
-     
     }, []);
 
     async function fetchMovies(){
-        const response = await fetch(
+        await fetch(
             url
             ).then((response) => response.json())
             .then((data) => setMovieData(data.results));
         }
 
-        const renderItem = ({ item }) => (
-            <Movie movieId={item.id} title={item.title} imageurl={item.poster_path} voteAvg={item.vote_average} />
-          );
-          
+    function updateSortMovies(){
+        setIsSorted(isSorted ? false : true);
+        const arr = [...movieData];
+        isSorted ? arr.sort((a,b) => {return a.vote_average - b.vote_average}) : arr.sort((a,b) => {return b.vote_average - a.vote_average});
+        setMovieData([...arr]);
+    }
+
+    const renderItem = ({ item }) => 
+    (
+        <Movie movieID={item.id} title={item.title} imageurl={item.poster_path} voteAvg={item.vote_average} />
+    );
+    
     return (
         <View style={styles.container}>
-            <Text style={styles.headline}>{type}</Text>
+            <View style={styles.headlineContainer}>
+                <Text style={styles.headline}>{type}</Text>
+                <TouchableOpacity onPress={updateSortMovies} style={styles.sortButton}>
+                    <Text style={styles.SortButtonText}>Sort by Rating  {isSorted ? '>': '<'}</Text>
+                </TouchableOpacity>
+            </View> 
             <FlatList
                 horizontal
+                extraData={movieData}
                 data={movieData}
                 renderItem={renderItem}
                 keyExtractor={(item) => item.id}
@@ -40,26 +53,24 @@ export default function MovieList({ type , url }){
             />
         </View>
     )
-
 }
 
-const Movie = ( {title, movieId, imageurl, voteAvg }) => 
+const Movie = ( {title, movieID, imageurl, voteAvg }) => 
     {
         const navigation = useNavigation();
+        const posterUrl = image_url+imageurl;
         return (
-    <TouchableOpacity style={styles.movieView} 
-        onPress={() => navigation.navigate("MovieDetails", {movieId})}>
-        <Image style={styles.poster}  source={{uri: image_url+imageurl}} >
-            </Image>
-        <Text
-            style={styles.title}
-        >
-            {title}
-        </Text>
-        <Text>
-            Rating: {voteAvg} / 10
-        </Text>
-    </TouchableOpacity>
+            <TouchableOpacity style={styles.movieView} 
+                onPress={() => navigation.navigate("MovieDetails", {movieID, posterUrl })}>
+                <Image style={styles.poster}  source={{uri: posterUrl}} >
+                </Image>
+                <Text style={styles.title}>
+                    {title}
+                </Text>
+                <Text>
+                    Rating: {voteAvg} / 10
+                </Text>
+            </TouchableOpacity>
   )};
 
   
@@ -68,8 +79,31 @@ const styles = StyleSheet.create({
     container:{
         height: 400,
         borderBottomColor: 'black',
-    borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomWidth: StyleSheet.hairlineWidth,
     },
+    headlineContainer:{
+        flexDirection: 'row',
+        padding: 10,
+        width: 500
+    },
+
+    sortButton: {
+        elevation: 4,
+        backgroundColor: "#009688",
+        borderRadius: 10,
+        paddingVertical: 5,
+        paddingHorizontal: 7,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+    SortButtonText: {
+        fontSize: 14,
+        color: "#fff",
+        fontWeight: "bold",
+        alignSelf: "center",
+        textTransform: "uppercase",
+      },
+
     headline:{
         fontSize: 28,
         margin: 10,
